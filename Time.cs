a24778c8dd22af7db70ee2fdc4de50ef07895823
Matan Kichler <44754991+matankic@users.noreply.github.com>
2022-04-27 20:17:29 +0300

    new class

---
+++ Time.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldWinForms
+{
+    public class Time
+    {
+        static int _day = 7, _hour = 0, _minute = 0;
+    }
+}
