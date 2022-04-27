434fb8596dd528d691c1b07ab3f4fc4d8ee65a8f
diamond-productions <102655492+diamond-productions@users.noreply.github.com>
2022-04-27 20:32:20 +0300

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+enum Sex{
+    Male,
+    Female
+}
+namespace HelloWorldWinForms
+{
+    internal class Animal
+    {
+        
+        private string _name;
+        private int _age, 
+            _thirst,
+            _hunger, 
+            _health,
+            _id;
+        private Sex _sex;
+        private bool _isFertile;
+
+
+    }
+}
---
+++ Farm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HelloWorldWinForms
+{
+    public class Farm 
+    {
+        static int credit;
+        public Employee[] employees =new Employee();
+        public Animal[] animals= new Animal();
+
+    }
+}
