5bafe8af90afef92ef91f6a7c7cf6d0d201e2f1d
Matan Kichler <44754991+matankic@users.noreply.github.com>
2022-04-27 19:27:10 +0300

    m

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,12 +39,14 @@
             this.label5 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_hello
             // 
             this.btn_hello.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.btn_hello.Location = new System.Drawing.Point(594, 37);
+            this.btn_hello.Location = new System.Drawing.Point(833, 33);
             this.btn_hello.Name = "btn_hello";
             this.btn_hello.Size = new System.Drawing.Size(89, 33);
             this.btn_hello.TabIndex = 0;
@@ -56,7 +58,7 @@
             // 
             this.lbl_click.AutoSize = true;
             this.lbl_click.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.lbl_click.Location = new System.Drawing.Point(710, 385);
+            this.lbl_click.Location = new System.Drawing.Point(949, 381);
             this.lbl_click.Name = "lbl_click";
             this.lbl_click.Size = new System.Drawing.Size(20, 24);
             this.lbl_click.TabIndex = 1;
@@ -66,7 +68,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label1.Location = new System.Drawing.Point(599, 384);
+            this.label1.Location = new System.Drawing.Point(838, 380);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(105, 25);
             this.label1.TabIndex = 2;
@@ -76,7 +78,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label2.Location = new System.Drawing.Point(599, 9);
+            this.label2.Location = new System.Drawing.Point(838, 5);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(57, 25);
             this.label2.TabIndex = 3;
@@ -86,7 +88,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label3.Location = new System.Drawing.Point(709, 9);
+            this.label3.Location = new System.Drawing.Point(948, 5);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(67, 25);
             this.label3.TabIndex = 4;
@@ -97,7 +99,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label4.Location = new System.Drawing.Point(710, 43);
+            this.label4.Location = new System.Drawing.Point(949, 39);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(36, 20);
             this.label4.TabIndex = 5;
@@ -108,7 +110,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label5.Location = new System.Drawing.Point(809, 9);
+            this.label5.Location = new System.Drawing.Point(1048, 5);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(80, 25);
             this.label5.TabIndex = 6;
@@ -118,7 +120,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.button1.Location = new System.Drawing.Point(594, 75);
+            this.button1.Location = new System.Drawing.Point(833, 71);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(89, 31);
@@ -130,20 +132,32 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label6.Location = new System.Drawing.Point(710, 79);
+            this.label6.Location = new System.Drawing.Point(949, 75);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(45, 20);
             this.label6.TabIndex = 8;
             this.label6.Text = "200$";
             this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::HelloWorldWinForms.Properties.Resources.grass_jpg;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(818, 578);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::HelloWorldWinForms.Properties.Resources.grass_jpg;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(885, 456);
+            this.ClientSize = new System.Drawing.Size(1136, 578);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
@@ -153,11 +167,12 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lbl_click);
             this.Controls.Add(this.btn_hello);
-            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Farm Simulator™";
             this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,6 +190,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
