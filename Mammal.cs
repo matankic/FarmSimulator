45c078bbcd2775ebdf73b76ba080a6bcc61b193f
diamond-productions <102655492+diamond-productions@users.noreply.github.com>
2022-05-03 22:04:11 +0300

    I Created New Classes, enjoy

---
+++ Mammal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldWinForms
+{
+    internal class Mammal : Animal
+    {
+        private bool _isLactating; //?
+        private int _Pragnancy;
+        
+
+    }
+}
+
+
