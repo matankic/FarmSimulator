57ae509efeb8e951c1070a65b551d359764329e4
Matan Kichler <44754991+matankic@users.noreply.github.com>
2023-08-31 18:27:41 +0300

    Update and rename Farm.cs to aquarium.cs
    m

--- Other classes/aquarium.cs
+++ Other classes/aquarium.cs
@@ -8,12 +8,12 @@ using System.Media;
 namespace HelloWorldWinForms
 {
     [Serializable]
-    internal class Farm 
+    internal class aquarium 
     {
         public List<Animal> myAnimals;
         private Credit myCredit;
         private Time myTime;
-        public int _farmSize;          
+        public int _aquariumSize;          
         public int _cnt_puff {get; set;}
         public int _cnt_seahorse {get; set;}
         public int _cnt_dolphin {get; set;}
@@ -22,12 +22,12 @@ namespace HelloWorldWinForms
         public int _cnt_jellyfish {get; set;}
 
         // protected Animal[] animals;
-        public Farm()
+        public aquarium()
         {
             myCredit = new Credit();
             myTime = new Time();
             myAnimals = new List<Animal>();
-            _farmSize = _cnt_puff = _cnt_seahorse = _cnt_dolphin = _cnt_turtle = _cnt_shark = _cnt_jellyfish = 0;
+            _aquariumSize = _cnt_puff = _cnt_seahorse = _cnt_dolphin = _cnt_turtle = _cnt_shark = _cnt_jellyfish = 0;
         }
         public ref Credit GetCreditRef() 
         {
@@ -40,27 +40,27 @@ namespace HelloWorldWinForms
         public void AddAnimalToList(int amount,int type) 
         {
             int i;
-            for (i = 0; i < amount; i++, _farmSize++)
+            for (i = 0; i < amount; i++, _aquariumSize++)
             {
                 switch (type)
                 {
                     case 0:
-                        myAnimals.Add(new puff(_farmSize));
+                        myAnimals.Add(new puff(_aquariumSize));
                         break;
                     case 1:
-                        myAnimals.Add(new turtle(_farmSize));
+                        myAnimals.Add(new turtle(_aquariumSize));
                         break;
                     case 2:
-                        myAnimals.Add(new seahorse(_farmSize));
+                        myAnimals.Add(new seahorse(_aquariumSize));
                         break;
                     case 3:
-                        myAnimals.Add(new dolphin(_farmSize));
+                        myAnimals.Add(new dolphin(_aquariumSize));
                         break;
                     case 4:
-                        myAnimals.Add(new shark(_farmSize));
+                        myAnimals.Add(new shark(_aquariumSize));
                         break;
                     case 5:
-                        myAnimals.Add(new jellyfish(_farmSize));
+                        myAnimals.Add(new jellyfish(_aquariumSize));
                         break;
                     default:
                         break;
